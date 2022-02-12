097b3b6644b808e28d9338bc09c85718971a5f46
jiramos87 <jiramos87@gmail.com>
2022-02-11 21:20:05 -0300

    first commit - folder structure

---
+++ src/views/Home.js
@@ -0,0 +1,26 @@
+const Home = () => {
+    return(
+        <div className="home-div">
+            <div className="blue-background">
+                
+            </div>
+            <div className="map-div">
+                    
+            </div>
+            <div className="controls-div">
+                <div className="page-title text-white">
+                    IP Address Tracker
+                </div>
+                <div className="ip-input">
+
+                </div>
+                <div className="results-div">
+
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Home
